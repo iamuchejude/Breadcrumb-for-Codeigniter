a0b4f17f829b5eec729815ffa485082b97df2fb6
Uche Jude <iamuchejude@gmail.com>
2018-06-05 21:31:02 +0100

    Updated README

--- config/breadcrumb.php
+++ config/breadcrumb.php
@@ -4,7 +4,7 @@
    * Config files for brreadcrumb generaterator
    * By Nwankwo Uche Jude(@JudeTheGenius)
   */
-  $config['includeHome'] = ''; // Title for site/app's first page
+  $config['includeHome'] = 'Home'; // Title for site/app's first page
   $config['divider'] = '&nbsp;&#62;&nbsp;'; // Crumb divider
   $config['containerOpen'] = '<div class="breadcrumb">'; // Opening tag for Breadcrumb container
   $config['containerClose'] = '</div>'; // Closing tag for Breadcrumb container
